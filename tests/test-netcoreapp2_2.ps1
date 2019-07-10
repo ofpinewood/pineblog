02f93498d396eef1bd2465c43b37a824867c3394
Peter van den Hout <peter@ofpinewood.com>
2019-07-10 13:36:21 +0200

    Add empty solution

--- tests/test-netcoreapp2_2.ps1
+++ tests/test-netcoreapp2_2.ps1
@@ -1,3 +1,3 @@
-dotnet build --configuration Release
-dotnet test --configuration Release --framework netcoreapp2.2 --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:Include="[Opw.*]*" /p:Exclude="[*.Tests]*%2c[*.*Tests]*"
+dotnet build ../PineBlog.sln --configuration Release
+dotnet test ../PineBlog.sln --configuration Release --framework netcoreapp2.2 --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:Include="[Opw.*]*" /p:Exclude="[*.Tests]*%2c[*.*Tests]*"
 PAUSE
