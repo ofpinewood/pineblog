b629bb61326772d49028dcf15966f9aac7e1e1e4
Peter van den Hout <peter@ofpinewood.com>
2019-07-10 16:17:58 +0200

    Add RazorUI library for blog UI

---
+++ src/Opw.PineBlog.RazorUI/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Opw.PineBlog.MyFeature.Pages
+{
+    public class Page1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ src/Opw.PineBlog.RazorUI/README.md
@@ -0,0 +1,1 @@
+https://docs.microsoft.com/en-us/aspnet/core/razor-pages/ui-class?view=aspnetcore-2.2&tabs=visual-studio
