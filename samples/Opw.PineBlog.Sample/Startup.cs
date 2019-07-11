0b1f92c71a76483468f99c21e1bca5bb891b599a
Peter van den Hout <peter@ofpinewood.com>
2019-07-11 21:53:13 +0200

    Add multiple dlls to the Opw.PineBlog.Core package

--- samples/Opw.PineBlog.Sample/Startup.cs
+++ samples/Opw.PineBlog.Sample/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Opw.PineBlog;
 using Opw.PineBlog.EntityFrameworkCore;
 
 namespace Opw.PineBlog.Sample
