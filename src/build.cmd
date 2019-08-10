763e5e032708f35f3d566e1db503e909a790750d
Peter van den Hout <peter@ofpinewood.com>
2019-08-10 20:57:01 +0200

    remove file

--- src/build.cmd
+++
@@ -1,4 +0,0 @@
-dotnet build Opw.PineBlog.Core\Opw.PineBlog.Core.csproj --configuration Release /p:Version=0.8.1.0
-
-dotnet pack Opw.PineBlog.Core\Opw.PineBlog.Core.csproj --no-build --output D:\a\1\a /p:Configuration=Release /p:PackageVersion=0.8.1-pullrequest0037-0012 --verbosity Detailed
-PAUSE
