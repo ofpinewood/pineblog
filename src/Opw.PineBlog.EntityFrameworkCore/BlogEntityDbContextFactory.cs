83bcdfcdb191e71d0600f0c34d6ec83a1d996173
Peter van den Hout <petervandenhout@users.noreply.github.com>
2019-08-11 15:57:26 +0200

    Feature/fixes3 (#41)
    * remove DbContextFactory
    * header update
    * PostDescriptionNotRequired

--- src/Opw.PineBlog.EntityFrameworkCore/BlogEntityDbContextFactory.cs
+++ src/Opw.PineBlog.EntityFrameworkCore/BlogEntityDbContextFactory.cs
@@ -3,11 +3,14 @@ using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Opw.EntityFrameworkCore;
 using System;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace Opw.PineBlog.EntityFrameworkCore
 {
     [Obsolete("This class is needed to run \"dotnet ef...\" commands from command line on development. Do not use directly.")]
-    public class BlogEntityDbContextFactory : DbContextFactory, IDesignTimeDbContextFactory<BlogEntityDbContext>
+    public class BlogEntityDbContextFactory : IDesignTimeDbContextFactory<BlogEntityDbContext>
     {
         public BlogEntityDbContext CreateDbContext(string[] args)
         {
@@ -17,6 +20,25 @@ namespace Opw.PineBlog.EntityFrameworkCore
             DbContextConfigurer.Configure(builder, connectionString);
 
             return new BlogEntityDbContext(builder.Options);
+        }
+
+        private IConfiguration GetConfiguration()
+        {
+            var projectDir = GetApplicationRoot();
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(projectDir)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+
+            return builder.Build();
+        }
+
+        private string GetApplicationRoot()
+        {
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
+            var regex = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+            var appRoot = regex.Match(path).Value;
+            return appRoot;
         }
     }
 }
--- src/Opw.PineBlog.EntityFrameworkCore/Migrations/20190811134804_PostDescriptionNotRequired.Designer.cs
+++ src/Opw.PineBlog.EntityFrameworkCore/Migrations/20190811134804_PostDescriptionNotRequired.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Opw.PineBlog.EntityFrameworkCore;
 
 namespace Opw.PineBlog.EntityFrameworkCore.Migrations
 {
     [DbContext(typeof(BlogEntityDbContext))]
-    partial class BlogEntityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190811134804_PostDescriptionNotRequired")]
+    partial class PostDescriptionNotRequired
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
