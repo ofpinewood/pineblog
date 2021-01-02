723d5783eef15fd04328aef7026e4c0f6f7d4839
Peter van den Hout <peter@ofpinewood.com>
2021-01-02 13:16:06 +0100

    GetPostByIdQuery tests

--- tests/Opw.PineBlog.Core.Tests/Posts/GetPostByIdQueryTests.cs
+++ tests/Opw.PineBlog.Core.Tests/Posts/GetPostByIdQueryTests.cs
@@ -1,80 +1,93 @@
-//using FluentAssertions;
-//using FluentValidation.Results;
-//using Microsoft.Extensions.DependencyInjection;
-//using Opw.PineBlog.Entities;
-//using Opw.HttpExceptions;
-//using System;
-//using System.Threading.Tasks;
-//using Xunit;
-//using System.Linq;
+using FluentAssertions;
+using FluentValidation.Results;
+using Opw.PineBlog.Entities;
+using Opw.HttpExceptions;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using System.Linq;
+using System.Collections.Generic;
+using Moq;
+using System.Linq.Expressions;
+using System.Threading;
 
-//namespace Opw.PineBlog.Posts
-//{
-//    public class GetPostByIdQueryTests : MediatRTestsBase
-//    {
-//        private Guid _postId;
+namespace Opw.PineBlog.Posts
+{
+    public class GetPostByIdQueryTests : MediatRTestsBase
+    {
+        private Guid _postId = Guid.NewGuid();
 
-//        public GetPostByIdQueryTests()
-//        {
-//            SeedDatabase();
-//        }
+        public GetPostByIdQueryTests()
+        {
+            var posts = new List<Post>();
+            posts.Add(new Post
+            {
+                Id = _postId,
+                AuthorId = Guid.NewGuid(),
+                Title = "Post title 0",
+                Slug = "post-title-0",
+                Description = "Description",
+                Content = "content with an url: %URL%/pineblog-tests/content-url-1. nice isn't it?",
+                CoverUrl = "%URL%/pineblog-tests/blog-cover-url",
+                Published = DateTime.UtcNow
+            });
 
-//        [Fact]
-//        public async Task Validator_Should_ThrowValidationErrorException()
-//        {
-//            Task action() => Mediator.Send(new GetPostByIdQuery());
+            PostRepositoryMock.Setup(m => m.SingleOrDefaultAsync(It.IsAny<Expression<Func<Post, bool>>>(), It.IsAny<CancellationToken>())).ReturnsAsync(posts.SingleOrDefault());
 
-//            var ex = await Assert.ThrowsAsync<ValidationErrorException<ValidationFailure>>(action);
-//            ex.Errors.Single(e => e.Key.Equals(nameof(GetPostByIdQuery.Id))).Should().NotBeNull();
-//        }
+            AddBlogUnitOfWorkMock();
+        }
 
-//        [Fact]
-//        public async Task Handler_Should_ReturnNotFoundException()
-//        {
-//            var result = await Mediator.Send(new GetPostByIdQuery { Id = Guid.NewGuid() });
+        [Fact]
+        public async Task Validator_Should_ThrowValidationErrorException()
+        {
+            Task action() => Mediator.Send(new GetPostByIdQuery());
 
-//            result.IsSuccess.Should().BeFalse();
-//            result.Exception.Should().BeOfType<NotFoundException<Post>>();
-//        }
+            var ex = await Assert.ThrowsAsync<ValidationErrorException<ValidationFailure>>(action);
+            ex.Errors.Single(e => e.Key.Equals(nameof(GetPostByIdQuery.Id))).Should().NotBeNull();
+        }
 
-//        [Fact]
-//        public async Task Handler_Should_ReturnPost()
-//        {
-//            var result = await Mediator.Send(new GetPostByIdQuery { Id = _postId });
+        [Fact]
+        public async Task Handler_Should_ReturnNotFoundException()
+        {
+            PostRepositoryMock.Setup(m => m.SingleOrDefaultAsync(It.IsAny<Expression<Func<Post, bool>>>(), It.IsAny<CancellationToken>())).ReturnsAsync(default(Post));
 
-//            result.IsSuccess.Should().BeTrue();
-//            result.Value.Title.Should().Be("Post title 0");
-//        }
+            AddBlogUnitOfWorkMock();
 
-//        private void SeedDatabase()
-//        {
-//            var context = ServiceProvider.GetRequiredService<BlogEntityDbContext>();
+            var result = await Mediator.Send(new GetPostByIdQuery { Id = Guid.NewGuid() });
 
-//            var author = new Author { UserName = "user@example.com", DisplayName = "Author 1" };
-//            context.Authors.Add(author);
-//            context.SaveChanges();
+            result.IsSuccess.Should().BeFalse();
+            result.Exception.Should().BeOfType<NotFoundException<Post>>();
+        }
 
-//            var post = CreatePost(0, author.Id);
-//            context.Posts.Add(post);
-//            context.SaveChanges();
+        [Fact]
+        public async Task Handler_Should_ReturnPost()
+        {
+            var result = await Mediator.Send(new GetPostByIdQuery { Id = _postId });
 
-//            _postId = post.Id;
-//        }
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Title.Should().Be("Post title 0");
+        }
 
-//        private Post CreatePost(int i, Guid authorId)
-//        {
-//            return new Post
-//            {
-//                AuthorId = authorId,
-//                Title = "Post title " + i,
-//                Slug = "post-title-" + i,
-//                Description = "Description",
-//                Content = "Content",
-//                Published = DateTime.UtcNow.AddDays(-30 + i),
-//                CoverUrl = "https://ofpinewood.com/cover-url",
-//                CoverCaption = "Cover caption",
-//                CoverLink = "https://ofpinewood.com/cover-link"
-//            };
-//        }
-//    }
-//}
+        [Fact]
+        public async Task Handler_Should_CoverUrl_ReplaceBaseUrlWithUrlFormat()
+        {
+            var result = await Mediator.Send(new GetPostByIdQuery { Id = _postId });
+
+            result.IsSuccess.Should().BeTrue();
+
+            result.Value.Should().NotBeNull();
+            result.Value.CoverUrl.Should().Be("http://127.0.0.1:10000/devstoreaccount1/pineblog-tests/pineblog-tests/blog-cover-url");
+        }
+
+        [Fact]
+        public async Task Handler_Should_UrlsInContent_ReplaceBaseUrlWithUrlFormat()
+        {
+            var result = await Mediator.Send(new GetPostByIdQuery { Id = _postId });
+
+            result.IsSuccess.Should().BeTrue();
+
+            result.Value.Should().NotBeNull();
+            result.Value.Content.Should().Be("content with an url: http://127.0.0.1:10000/devstoreaccount1/pineblog-tests/pineblog-tests/content-url-1. nice isn't it?");
+        }
+    }
+}
