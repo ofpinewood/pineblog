305cb6e340dd982668244f1a0aa6bea20dcc094f
Peter van den Hout <peter@ofpinewood.com>
2021-01-10 08:06:37 +0100

    WIP

--- src/Opw.PineBlog.EntityFrameworkCore/BlogEntityDbContextFactory.cs
+++ src/Opw.PineBlog.EntityFrameworkCore/BlogEntityDbContextFactory.cs
@@ -1,45 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
 using System;
 using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using System.Reflection;
-using System.Text.RegularExpressions;
 
 namespace Opw.PineBlog.EntityFrameworkCore
 {
     [ExcludeFromCodeCoverage]
     [Obsolete("This class is needed to run \"dotnet ef...\" commands from command line on development. Do not use directly.")]
-    public class BlogEntityDbContextFactory : IDesignTimeDbContextFactory<BlogEntityDbContext>
+    internal class BlogEntityDbContextFactory : IDesignTimeDbContextFactory<BlogEntityDbContext>
     {
+        private const string DesignTimeConnectionString = "Server=.\\SQLEXPRESS; Database=pineblog-db; Trusted_Connection=True;";
+
         public BlogEntityDbContext CreateDbContext(string[] args)
         {
             var builder = new DbContextOptionsBuilder<BlogEntityDbContext>();
-
-            var connectionString = GetConfiguration().GetConnectionString("DefaultConnection");
-            DbContextOptionsHelper.ConfigureOptionsBuilder(builder, connectionString);
-
+            DbContextOptionsHelper.ConfigureOptionsBuilder(builder, DesignTimeConnectionString);
             return new BlogEntityDbContext(builder.Options);
-        }
-
-        private IConfiguration GetConfiguration()
-        {
-            var projectDir = GetApplicationRoot();
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(projectDir)
-                .AddJsonFile("appsettings.json")
-                .AddEnvironmentVariables();
-
-            return builder.Build();
-        }
-
-        private string GetApplicationRoot()
-        {
-            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
-            var regex = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
-            var appRoot = regex.Match(path).Value;
-            return appRoot;
         }
     }
 }
