0aa9b7faf818c22424fc6ecf07ed580e461f6cef
Peter van den Hout <peter@ofpinewood.com>
2021-08-17 18:31:55 -0500

    Add Opw.PineBlog.Sample.GitDb project

---
+++ samples/Opw.PineBlog.Sample.GitDb/Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Opw.PineBlog.Sample.GitDb.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
